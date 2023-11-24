f88a6cd42eb2ba966aaccc7423c7032ca799d5f6
민복기( MIN BOK KI ) <1632615@mobis.co.kr>
2023-11-24 11:37:05 +0900

    Fix iris-workflow.yaml

--- load_iris.py
+++ load_iris.py
@@ -7,7 +7,7 @@ def load_data():
 
 def prepare_data(data, label):
     from sklearn.model_selection import train_test_split
-    train_X, test_X, train_y, test_y = train_test_split(data.values, label.reshape(-1,1), test_size=0.2, random_state=2023)
+    train_X, test_X, train_y, test_y = train_test_split(data.values, label.values.reshape(-1,1), test_size=0.2, random_state=2023)
 
     return train_X, test_X, train_y, test_y
 
---
+++ main.py
@@ -0,0 +1,5 @@
+from load_iris import *
+
+if __name__ == '__main__':
+    print("hi")
+    model_pipeline(*prepare_data(*load_data()))
